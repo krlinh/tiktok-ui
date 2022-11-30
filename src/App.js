0a6c501eac23341d96913599fbe4711eea73067a
KarenLinh <dlinhks94@gmail.com>
2022-11-30 16:24:02 +0700

    c5 - Cấu hình Router/Layout cho dự án

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from '~/routes/index';
+import { DefaultLayout } from '~/components/Layouts';
+import { Fragment } from 'react';
 function App() {
     return (
         <Router>
             <div className="App">
                 <Routes>
                     {publicRoutes.map((route, index) => {
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
                         const Page = route.component;
-                        return <Route key={index} path={route.path} element={<Page />} />;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
                     })}
                 </Routes>
             </div>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,14 @@
+import { HeaderOnly } from '~/components/Layouts';
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/following', component: Following },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
 ];
 
 const privateRoutes = [];
